7e9538f69ebfb156ab62d718b1f7289579e35e2b
LaurensIsta <laurens-i@Hotmail.com>
2018-06-06 13:03:49 +0200

    Index commit
    FIrst commit of the index

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 //TODO: Find out how to add extra dependencies
 const functions = require('firebase-functions');
 
-
 exports.webhook = functions.https.onRequest((request, response) => {
     //All logic here
     receivedText = request.body.queryResult.queryText;
